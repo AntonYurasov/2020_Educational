3e6118ecac4ddbfb2a2d4da4494984171e0933d3
Artem <pachevartem@ya.ru>
2020-04-21 23:24:01 +0300

    new behaviour

---
+++ Assets/Scripts/ClliderController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ClliderController : MonoBehaviour
+{
+    private Transform pointA;
+    private Transform pointB;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.transform.name);
+    }
+
+    private GameObject a;
+    private void Update()
+    {
+        #region Вращение объекта вокруг осей
+
+        // var h = Input.GetAxisRaw("Horizontal");
+        // a.transform.Rotate(Vector3.up*h);
+
+        #endregion
+      
+        // Блок вращения объектов
+        var h = Input.GetAxisRaw("Horizontal");
+
+
+    }
+
+   
+    
+    
+}
+
+
+public static class Ex
+{
+   public static Vector3 RandomPoint(Transform a, Transform b)
+    {
+        var _x = Random.Range(a.transform.position.x, b.transform.position.x);
+        var _y = Random.Range(a.transform.position.y, b.transform.position.y);
+        var _z = Random.Range(a.transform.position.z, b.transform.position.z);
+        
+        return new Vector3(_x,_y,_z);
+    }
+
+   public static bool ChkDistance(Transform a, Transform b, float dist)
+   {
+       return Vector3.Distance(a.transform.position, b.transform.position) < dist;
+   }
+}
---
+++ Assets/Scripts/FlappyBirds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlappyBirds : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Bird;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = Bird.AddComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, _rigidbody.velocity.y, 1);
+
+        if (Input.GetKeyDown(KeyCode.Insert))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.AddForce(Vector3.up*10,ForceMode.Impulse);
+        }
+    }
+}
